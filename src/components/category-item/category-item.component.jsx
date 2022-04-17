6d067d487b842508c7e78dfc2872f822fce88f6b
Katherine Ebel <kathyebel@me.com>
2022-04-17 15:25:30 -0400

    implement directory component with categories

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,15 @@
+export default function CategoryItem ({ category }) {
+  const { id, imageUrl, title } = category
+  return (
+    <div key={id} className="category-container">
+      {/*<img src="" alt=""/>*/}
+      <div className="background-image"
+           style={{backgroundImage: `url(${imageUrl})`}}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  )
+}
